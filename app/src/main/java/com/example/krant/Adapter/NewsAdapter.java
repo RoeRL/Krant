064fd7a0c471e9551dcce496f50e83c90535780a
Ardiansa Firnando <ardiansa312@gmail.com>
2021-10-15 23:06:37 +0700

    feat/[AF] Recyclerview Detail Activity

--- app/src/main/java/com/example/krant/Adapter/NewsAdapter.java
+++ app/src/main/java/com/example/krant/Adapter/NewsAdapter.java
@@ -8,6 +8,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.krant.Model.NewsModel;
@@ -18,12 +19,20 @@ import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
+import javax.security.auth.callback.Callback;
+
 public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.NewsViewHolder> {
 
     ArrayList<NewsModel> listData;
+    Callback callback;
 
-    public NewsAdapter(ArrayList<NewsModel> dataArrayList) {
-        listData = dataArrayList;
+    public interface Callback{
+        void onClick(int position);
+    }
+
+    public NewsAdapter(ArrayList<NewsModel> dataArrayList, Callback callback) {
+        this.listData = dataArrayList;
+        this.callback = callback;
     }
 
     @NonNull
@@ -37,7 +46,7 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.NewsViewHolder
     @Override
     public void onBindViewHolder(@NonNull NewsAdapter.NewsViewHolder holder, int position) {
 
-        holder.txt_name.setText(listData.get(position).getName());
+        holder.txt_title.setText(listData.get(position).getTitle());
         holder.txt_description.setText(listData.get(position).getDescription());
         Picasso.get()
                 .load(listData.get(position).getImage())
@@ -51,16 +60,25 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.NewsViewHolder
 
     public class NewsViewHolder extends RecyclerView.ViewHolder{
 
-        private TextView txt_name;
+        private TextView txt_title;
         private TextView txt_description;
         private ImageView img_image;
+        private CardView cardView;
 
         public NewsViewHolder(@NonNull View itemView) {
             super(itemView);
 
-            txt_name = itemView.findViewById(R.id.txt_name_news);
+            txt_title = itemView.findViewById(R.id.txt_title_news);
             txt_description = itemView.findViewById(R.id.txt_description_news);
             img_image = itemView.findViewById(R.id.img_image_news);
+            cardView = itemView.findViewById(R.id.id_cardview);
+
+            cardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    callback.onClick(getLayoutPosition());
+                }
+            });
         }
     }
 }
---
+++ app/src/main/java/com/example/krant/DetailActivity.java
@@ -0,0 +1,44 @@
+package com.example.krant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+public class DetailActivity extends AppCompatActivity {
+
+    private Bundle bundle;
+    private ImageView img_image;
+    private TextView txt_title;
+    private TextView txt_description;
+
+    private String title;
+    private String description;
+    private String image;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        txt_title = findViewById(R.id.txt_title_detail);
+        txt_description = findViewById(R.id.txt_description_detail);
+        img_image = findViewById(R.id.img_image_detail);
+
+        bundle = getIntent().getExtras();
+        if (bundle != null){
+            title = bundle.getString("title");
+            description = bundle.getString("description");
+            image = bundle.getString("imgae");
+        }
+
+        txt_title.setText(title);
+        txt_description.setText(description);
+        Picasso.get()
+                .load(image)
+                .into(img_image);
+    }
+}
